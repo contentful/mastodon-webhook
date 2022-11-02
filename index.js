289ddceb477a06d85e3aa1dd81dd5603c0a40c5e
Alvin <hello@alvin.codes>
2022-11-02 20:16:22 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import contentful from 'contentful-management';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const { CMA_TOKEN, SPACE_ID, MASTODON_TOKEN, MASTODON_URL } = process.env;
+
+const endpoint = `${MASTODON_URL}/api/v1/statuses?access_token=${MASTODON_TOKEN}`;
+
+const client = contentful.createClient({
+  accessToken: CMA_TOKEN,
+});
+
+// Create webhook
+client
+  .getSpace(SPACE_ID)
+  .then((space) => {
+    return space.createWebhook({
+      name: 'Post to Mastodon',
+      url: endpoint,
+      topics: ['Entry.publish'],
+      filters: [
+        {
+          in: [
+            {
+              doc: 'sys.environment.sys.id',
+            },
+            ['master'],
+          ],
+        },
+      ],
+      transformation: {
+        contentType: 'application/json',
+        // body: '', // the "body" property is for custom payload, see the blog post for an example
+      },
+    });
+  })
+  .then((webhook) => console.log(webhook))
+  .catch(console.error);
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Contentful webhook for Mastodon
+
+Everything that is in this repo can be done using the UI.
+
+## Installation
+
+1. Install dependencies: `npm install`
+2. Rename `.env.example` and add the environment variables required. See the blog post for more information
